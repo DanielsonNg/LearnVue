ef55f772d5118d4cfe7ad72f588b339d66db8914
unknown <danielson7632@gmail.com>
2025-02-07 13:21:55 +0700

    conditional rendering

---
+++ src/components/TodoList.vue
@@ -0,0 +1,36 @@
+<script setup>
+
+const items = [
+    {
+        id: 1,
+        text: "Learn Vue",
+        deadline: 1,
+        done: true
+    },
+    {
+        id: 2,
+        text: "Learn Python",
+        deadline: 10,
+        done: false
+    },
+    {
+        id: 3,
+        text: "Learn React",
+        deadline: 30,
+        done: true
+    }
+]
+</script>
+
+<template>
+    <h1>Todo List</h1>
+    <template v-for="item in items">
+        <ul :key="item.id" v-if="item.done">
+            <li v-for="(value, key) in item">
+                {{ key + ': ' + value }}
+            </li>
+        </ul>
+    </template>
+    <h3 v-for="n in 3">Hello {{ n }}</h3>
+</template>
+<style></style>
