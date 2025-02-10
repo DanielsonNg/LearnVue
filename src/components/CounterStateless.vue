8a8c1292a9989f66387c4cb216c2996cfbc89ae8
unknown <danielson7632@gmail.com>
2025-02-10 10:56:51 +0700

    component props event handling

--- src/components/CounterStateless.vue
+++ src/components/CounterStateless.vue
@@ -1,7 +1,17 @@
 <script setup>
-const { counter, name } = defineProps(['counter', "name"])
+const { counter, name, increment } = defineProps({
+    counter: Number,
+    name: String,
+    increment: Number
+})
+const emits = defineEmits(["click"])
+
+function buttonClick(){
+    emits('click', increment)
+}
 </script>
 
 <template>
     {{ name }} : {{ counter }}
+    <button @click="buttonClick">Increment {{ name }}</button>
 </template>
--- src/components/MultiCounter.vue
+++ src/components/MultiCounter.vue
@@ -5,13 +5,27 @@ import CounterStateless from './CounterStateless.vue';
 
 const counter = ref(0)
 
+function increment(value){
+    counter.value += value
+}
+
 </script>
 
 <template>
     <Counter name="Dan" initialCount=0 />
     <Counter name="Ben" initialCount=1 />
 
-    <CounterStateless name="Jane" :counter="counter" /><br>
-    <CounterStateless name="Doe" :counter="counter" /><br>
+    <CounterStateless 
+    @click="() => increment" 
+    name="Jane" 
+    :counter="counter"
+    increment="2"
+    /><br>
+
+    <CounterStateless 
+    @click="() => increment"
+    name="Doe" 
+    :counter="counter"
+    :increment="1" /><br>
     <button @click="counter++">Increment Counter</button>
 </template>
